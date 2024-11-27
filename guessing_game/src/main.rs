9e97f4ff1c8ee6c6087d2170f9ca101ed4462818
Mehulyadavv <mehulyadav432@gmail.com>
2024-11-27 12:40:02 +0000

    added basic functionalities

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,5 +4,11 @@ fn main() {
     println!("Guess the number!");
     println!("Input your number");
 
-    let mut
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read");
+
+    println!("you guessed: {}", guess);
 }
